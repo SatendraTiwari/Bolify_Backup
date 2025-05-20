28d499c320792e8137a7a5a788e133229eeab996
SatendraTiwari <satendrasharma1953@gmail.comgit config --global user.name SatendraTiwarigit config --global user.name SatendraTiwarigit config --global user.email satendrasharma1953@gmail.com>
2025-05-20 21:32:46 +0530

    jwt v04

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -2,6 +2,12 @@ export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
   res
     .status(statusCode)
+    .cookies("token", token, {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+    })
     .cookie("token", token, {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000

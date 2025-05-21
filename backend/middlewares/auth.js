dbbd7d6cfa1aa4ba6d619633308121a58edcde60
SatendraTiwari <satendrasharma1953@gmail.comgit config --global user.name SatendraTiwarigit config --global user.name SatendraTiwarigit config --global user.email satendrasharma1953@gmail.com>
2025-05-21 08:03:29 +0530

    jwt v08 auth v08

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,10 +4,11 @@ import ErrorHandler from "./error.js";
 import { catchAsyncErrors } from "./catchAsyncErrors.js";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const token = req.cookies.token || req.headers.authorization?.replace("Bearer ", "");
-  
+  const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.startsWith("Bearer ") && req.headers.authorization.split(" ")[1]);
+
   console.log(res);
   console.log("Token:", token);
+  
   if (!token) {
     return next(new ErrorHandler("User not authenticated.", 400));
   }
--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -6,7 +6,9 @@ export const generateToken = (user, message, statusCode, res) => {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
-      httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true
     })
     .setHeader('Authorization', `Bearer ${token}`)
     .json({
